49d530a7da1a32d806d6000d9824eb5fc6022689
Nate McMaster <nate.mcmaster@microsoft.com>
2018-03-05 14:50:28 -0800

    Compare default and managed

--- benchmarks/PBKDF2Benchmarks/HashBenchmarks.cs
+++ benchmarks/PBKDF2Benchmarks/HashBenchmarks.cs
@@ -2,6 +2,7 @@ using System.Security.Cryptography;
 using System.Text;
 using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation.PBKDF2;
 [assembly: BenchmarkDotNet.Attributes.AspNetCoreBenchmark]
 namespace PBKDF2Benchmark
 {
@@ -10,6 +11,8 @@ namespace PBKDF2Benchmark
         [Params(1000, 10000, 100000)]
         public int Iterations { get; set; }
         private static readonly byte[] _salt = GenerateSalt();
+        private static readonly DefaultPbkdf2Provider _defaultProvider = new DefaultPbkdf2Provider();
+        private static readonly ManagedPbkdf2Provider _managedProvider = new ManagedPbkdf2Provider();
 
         private static byte[] GenerateSalt()
         {
@@ -28,10 +31,18 @@ namespace PBKDF2Benchmark
             var rfc = new Rfc2898DeriveBytes(_password, _salt, Iterations, HashAlgorithmName.SHA512);
             return rfc.GetBytes(64);
         }
+
         [Benchmark]
-        public byte[] KeyDerivationPBKDF2SHA512()
+        public byte[] ManagedPbkdf2ProviderSHA512()
         {
-            var key = KeyDerivation.Pbkdf2("Hello World", _salt, KeyDerivationPrf.HMACSHA512, Iterations, 512 / 8);
+            var key = _managedProvider.DeriveKey("Hello World", _salt, KeyDerivationPrf.HMACSHA512, Iterations, 512 / 8);
+            return key;
+        }
+
+        [Benchmark]
+        public byte[] DefaultPbkdf2ProviderSHA512()
+        {
+            var key = _defaultProvider.DeriveKey("Hello World", _salt, KeyDerivationPrf.HMACSHA512, Iterations, 512 / 8);
             return key;
         }
     }
